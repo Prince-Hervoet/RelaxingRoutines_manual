c6b56949256f58071e3aaf40ca8c36b5213da0e9
jammer <1378239869@qq.com>
2023-05-25 19:23:11 +0000

    fix: #add codes

--- src/routine.hpp
+++ src/routine.hpp
@@ -44,31 +44,3 @@ public:
         }
     }
 };
-
-// class RoutineHandler
-// {
-//     friend RoutineHandler *simple_new(TaskFunc task, void *args);
-//     friend int simple_resume(RoutineProcess *routine);
-//     friend void simple_await();
-//     friend void t_simple_pause();
-//     friend class Controller;
-//     friend void threadFunc(void *args);
-
-// public:
-//     RoutineHandler()
-//     {
-//     }
-//     RoutineHandler(RoutineProcess *pro, RoutineHandler *father)
-//     {
-//         routine = pro;
-//     }
-
-//     ~RoutineHandler()
-//     {
-//         delete routine;
-//     }
-
-// private:
-//     RoutineHandler *father;
-//     RoutineProcess *routine;
-// };
